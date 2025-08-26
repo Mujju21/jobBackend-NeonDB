0252a4222d5ef924896c0813b325d60ac320bca3
unknown <mujamillshaikh@gmail.com>
2025-08-26 23:47:28 +0530

    Added CRUD Request

--- server.js
+++ server.js
@@ -12,14 +12,33 @@ app.use(cors());
 const sql = neon(process.env.DB_URL);
 
 app.get('/', (req,res) => {
-    return res.send("Okkk");
+    return res.send("Jobs backend API is running");
 });
 
+//read all jobs
 app.get('/jobs',async (req,res) => {
     const jobs = await sql`SELECT * FROM tbljobs`;
     return res.status(200).json({jobs})
 });
 
+//Read job by ID
+app.get('/jobs/:id', async(req,res) => {
+    const {id} = req.params
+    try{
+    const jobs = await sql`SELECT * FROm tbljobs WHERE id=${id}`
+
+    if (jobs.length === 0) {
+        return res.status(404).json({ message: "Job not found" });
+    }
+
+    return res.status(200).json({jobs: jobs[0]})
+    }catch(error){
+        console.error("Error fetching job by ID:", error);
+        return res.status(500).json({ error: "Failed to fetch job" });
+    }
+});
+
+//Add Job
 app.post('/jobs', async(req,res) => {
     try{
         const{  
@@ -41,7 +60,7 @@ app.post('/jobs', async(req,res) => {
         RETURNING *;
         `;
         return res.status(201).json({
-            message:"JOb ADDED DONE",
+            message:"JOB ADDED DONE",
             jobs: jobs[0]
         });
         }catch(error){
@@ -50,6 +69,63 @@ app.post('/jobs', async(req,res) => {
         }
 });
 
-const PORT = 3000 || 4000;
-app.listen(PORT, ()=> console.log(`runnnig on server: ${PORT}`)
-)
+//Update Job
+app.put('/jobs/:id', async (req,res) => {
+    const {id} = req.params;
+    try{
+        const{  
+            title,
+            type,
+            location,
+            description,
+            salary,
+            company_name,
+            company_desc,
+            company_email,
+            company_phone
+        } = req.body;
+        const jobs = await sql` UPDATE tbljobs SET 
+        title = ${title},
+        type = ${type},
+        location = ${location},
+        description = ${description},
+        salary = ${salary},
+        company_name = ${company_name},
+        company_desc = ${company_desc},
+        company_email = ${company_email},
+        company_phone = ${company_phone}
+        WHERE id = ${id}
+        RETURNING *;
+        `;
+        if (jobs.length === 0) {
+            return res.status(404).json({ message: "Job not found" });
+         };
+        return res.status(200).json({
+            message: "Job updated successfully",
+            jobs : jobs[0]
+        });
+    }catch(error){
+        return res.status(500).json({error:"Failed to update job"});
+    }
+});
+
+//Delete Job
+app.delete('/jobs/:id', async(req,res) => {
+    const {id} = req.params;
+    try{
+    const jobs = await sql` DELETE FROM tbljobs WHERE id=${id} RETURNING *`
+    if (jobs.length === 0) {
+        return res.status(404).json({ message: "Job not found" });
+    };
+    return res.status(200).json({
+        message:"Job deleted successfully",
+        jobs:jobs[0]
+    });
+    }catch(error){
+        return res.status(500).json({error:"Failed to delete job"})
+}
+});
+
+const PORT = 7000 || 8000;
+app.listen(PORT, ()=> console.log(`runnnig on server: http://localhost:${PORT}`)
+);
